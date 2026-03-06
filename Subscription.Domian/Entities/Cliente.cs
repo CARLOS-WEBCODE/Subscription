4ca7522280bc3aba29158834251608b524c2203a
Antonio Barbosa <acrb1108@gmail.com>
2026-03-06 00:14:18 -0300

    feat(Subscripton.Domain): adiconando as classes do Entities.

---
+++ Subscription.Domian/Entities/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using Subscription.Domain.Enums;
+using Subscription.Domian.Commons;
+using Subscription.Domian.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subscription.Domain.Entities
+{
+    public class Cliente : BaseEntity
+    {
+        #region Propriedades
+        public string Nome { get; set; } = string.Empty;
+        public Email? Email { get; set; }
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+        public StatusCliente Status { get; set; } = StatusCliente.Ativo;
+        #endregion
+
+        #region Relacionamentos
+        public ICollection<Assinatura>? Assinaturas { get; set; }
+        #endregion
+
+    }
+
+}
---
+++ Subscription.Domian/Entities/Plano.cs
@@ -0,0 +1,24 @@
+﻿using Subscription.Domain.Enums;
+using Subscription.Domian.Commons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subscription.Domain.Entities
+{
+    public class Plano : BaseEntity
+    {
+        #region Propriedades
+        public string Nome { get; set; } = string.Empty;
+        public decimal ValorMensal { get; set; }
+        public Periodicidade? Periodicidade { get; set; }
+        public bool Ativo { get; set; }
+        #endregion
+
+        #region Relacionamentos
+
+        public ICollection <Assinatura>? Assinaturas { get; set; }
+
+        #endregion
+    }
+}

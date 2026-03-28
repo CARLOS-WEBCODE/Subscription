aa32937217c76c9cc0c1ba0ca09f4773a1498081
Antonio Barbosa <acrb1108@gmail.com>
2026-03-28 19:44:50 -0300

    feat: Interfaces (IPlanoRepository, IClienteRepository, IAssinaturaRepository)

---
+++ Subscription.Domian/Interfaces/Repositories/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Subscription.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subscription.Domain.Interfaces.Repositories
+{
+    public interface IClienteRepository : IBaseRepository<Cliente, Guid>
+    {
+
+    }
+}
+
---
+++ Subscription.Domian/Interfaces/Repositories/IPlanoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Subscription.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subscription.Domain.Interfaces.Repositories
+{
+    public interface IPlanoRepository : IBaseRepository<Plano, Guid>
+    {
+
+    }
+}
+
